4894c8f1b539e9d4ebdaff50cb084bb5b320e2b9
holystar97 <35961898+holystar97@users.noreply.github.com>
2019-12-10 21:26:14 +0900

    Update MemberDTO.java

--- work/MemberDTO.java
+++ work/MemberDTO.java
@@ -1,5 +1,10 @@
 package com.javateam.jsp.vo;
 
+import java.lang.reflect.Field;
+import java.util.Iterator;
+import java.util.Map;
+import java.util.Set;
+
 /**
  * DTO(Data Transfer Object), TO
  * 
@@ -37,6 +42,53 @@ public class MemberDTO {
 		this.address = address;
 	}
 	
+	
+    /**
+     * Map 형태(request.getParameterMap())로 인자를 받는 경우 인자 입력처리 생성자
+     *  
+     * @param map 인자 맵(Map)
+     */
+    public MemberDTO(Map<String, String[]> map) {
+       
+        System.out.println("인자 입력 처리");
+       
+        Set<String> set = map.keySet();
+        Iterator<String> it = set.iterator();
+        Field field; // reflection 정보 활용
+       
+        while (it.hasNext()) {
+           
+            String fldName = it.next();
+           
+            if (!fldName.equals("submitBtn"))  { // 버튼 필드 제외
+               
+                try {
+                        // DTO(MemberDTO)와 1:1 대응되는 필드들 처리
+                        try {
+                                field = this.getClass().getDeclaredField(fldName);
+                                System.out.println(fldName + " : "+field.getName());
+                                field.setAccessible(true);
+                                // System.out.println("field : "+field);
+                               
+                                    field.set(this, map.get(fldName)[0]);
+                     
+                               
+                        } catch (NoSuchFieldException e) {
+                           
+                            // 만약 VO와 1:1 대응되지 않는 인자일 경우는 이 부분에서 입력처리합니다.
+                            System.out.println("인자와 필드가 일치하지 않습니다.");
+                           
+                        } // try
+                       
+                } catch (SecurityException | IllegalArgumentException | IllegalAccessException e) {
+                    e.printStackTrace();
+                } // try
+           
+            } // if
+           
+        } // while
+       
+    } //
 	
 	//getter"s": 객체의 멤버 필드 들의 내용을 한꺼번에 확인
 	@Override
