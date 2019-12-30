908bc288126f52bac89cd64a8151c9d55be01bf1
holystar97 <35961898+holystar97@users.noreply.github.com>
2019-12-30 21:26:26 +0900

    Create MemberDAO.java

---
+++ memberproject/MemberDAO.java
@@ -0,0 +1,13 @@
+package com.javatea.member_project2.dao;
+
+import com.javatea.member_project2.domain.MemberVO;
+
+public interface MemberDAO {
+
+	boolean insertMember(MemberVO member) throws Exception;
+	
+	
+	
+	
+	
+}
