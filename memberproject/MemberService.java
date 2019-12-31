049129bf63f08f9dcf2e1533021d023891e753dc
holystar97 <35961898+holystar97@users.noreply.github.com>
2019-12-31 20:22:04 +0900

    Update MemberService.java

--- memberproject/MemberService.java
+++ memberproject/MemberService.java
@@ -16,7 +16,16 @@ public interface MemberService {
 	// 오버라이드 조건 : 추상 함수도 같이 예외 throw를 맞추어주어야한다 
 	// connection을 조정한다 
 	
+	/**
+	 * 개별 회원정보 조회
+	 * @param memberId 회원 아이디
+	 * @return 회원정보객체
+	 * @throws Exception 예외처리
+	 * 
+	 * */
 	
+	
+	MemberVO getMember(String memberId) throws Exception;
 	
 	
 	
