6d231ab29004a36e4af1f40c4aa64cba3e9fa3ea
holystar97 <35961898+holystar97@users.noreply.github.com>
2019-12-31 20:20:52 +0900

    Update MemberDAO.java

--- memberproject/MemberDAO.java
+++ memberproject/MemberDAO.java
@@ -4,7 +4,30 @@ import com.javatea.member_project2.domain.MemberVO;
 
 public interface MemberDAO {
 
+	
+	/**
+	 * 회원정보생성 
+	 * 
+	 * @param member 회원정보 객체
+	 * @return 저장 성공여부
+	 * @throws Exception 예외처리
+	 * 
+	 * */
+	
 	boolean insertMember(MemberVO member) throws Exception;
+	
+	
+	/**
+	 * 개별 회원정보 조회
+	 * 
+	 * @param memberId 회원 아이디
+	 * @return 회원정보 객체
+	 * @throws Exception 예외처리
+	 * */
+	
+	
+	MemberVO getMember(String memberId) throws Exception;
+	
 	
 	
 	
