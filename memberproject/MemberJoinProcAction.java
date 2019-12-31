0f21b2939aa2adecb9421cfc439115893d83cbe3
holystar97 <35961898+holystar97@users.noreply.github.com>
2019-12-31 20:20:14 +0900

    Update MemberJoinProcAction.java

--- memberproject/MemberJoinProcAction.java
+++ memberproject/MemberJoinProcAction.java
@@ -3,6 +3,7 @@ package com.javateam.member_project2.action;
 import java.io.IOException;
 import java.sql.Date;
 
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -10,6 +11,8 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import com.javatea.member_project2.domain.MemberVO;
+import com.javatea.member_project2.service.MemberService;
+import com.javatea.member_project2.service.MemberServiceImpl;
 
 /**
  * Servlet implementation class MemberJoinProcAction
@@ -40,6 +43,10 @@ public class MemberJoinProcAction extends HttpServlet {
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
 		System.out.println("회원가입 처리");
+		
+		boolean flag=false; //성공 실패 나누려고 -> 회원정보 생성 성공 여부 
+		String returnPath= "/result/result.jsp"; // 이동 페이지 
+		String msg="";  // 결과 메시지 변수
 		
 		// 인자 처리 :출력 
 		
@@ -73,15 +80,42 @@ public class MemberJoinProcAction extends HttpServlet {
 		System.out.println("MemberVO "+ member);
 		
 		
-		// vo -> DAO
+		// vo ->Service ( => DAO)  삼중구조 
+		
+		MemberService service =new MemberServiceImpl();
+		
+		
+		// 성공/실패 -> 페이지 이동 
+		try {
+			flag=service.insertMember(member);
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			flag=false;
+		}
+		
+		if(flag==true) {
+			msg="회원정보 저장에 성공하였습니다.";
+			//returnPath="";
+			
+		}else {
+			//returnPath="";
+			msg="회원정보 저장에 실패하였습니다.";
+			
+		
+		}
+		
+		//메시지 전송 
+		
+		request.setAttribute("msg", msg);
+		
 		
 		//datbase 에 값 넣기 
 		
 		
-		
-		
-		// 성공/실패 -> 페이지 이동 
-		
+		// 보내기 
+		RequestDispatcher rd=request.getRequestDispatcher(returnPath);
+		rd.forward(request, response);
 		
 		
 		
